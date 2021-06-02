db333a3e81f753565dec1053484ed7f467e35b32
Otto-TV <85245680+Otto-TV@users.noreply.github.com>
2021-06-03 02:26:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                    InlineKeyboardButton("Stream TV ‼️", url="https://t.me/StreamTVCommunity"),
+                    InlineKeyboardButton("About Me 😎", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel 🤖", url="https://t.me/OPBots"),
+                    InlineKeyboardButton("Support Group 🙆", url="https://t.me/RequestStreamTV")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE 😂", url="https://github.com")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
