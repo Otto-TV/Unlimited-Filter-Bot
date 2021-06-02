0c3ce092d7821ddd92c6feb6b29ca6c2ff76c50b
Otto-TV <85245680+Otto-TV@users.noreply.github.com>
2021-06-03 02:22:30 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                    InlineKeyboardButton("Stream TV ‼️", url="https://t.me/StreamTVCommunity"),
+                    InlineKeyboardButton("About Me 😎", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel 🤖", url="https://t.me/OPBots"),
+                    InlineKeyboardButton("Support Group 🙆", url="https://t.me/RequestStreamTV")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE 😂", url="https://github.com/")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
