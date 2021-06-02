27c500fdab79d56e39f6e19596487f6f5ff1c60b
Otto-TV <85245680+Otto-TV@users.noreply.github.com>
2021-06-03 02:06:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 class Script(object):
 
-    START_MSG = """<b>Hy {},
+    START_MSG = """<b>Hey {},
 
-I'm an advanced filter bot with many capabilities!
+ 😎 I'm an advanced filter bot with many capabilities!
 There is no practical limits for my filtering capacity :)
 
 See <i>/help</i> for commands and more details.</b>
